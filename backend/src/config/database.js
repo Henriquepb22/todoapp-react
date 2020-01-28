eb5f3e57f54c5db569e2aed4c961ad5043b3ecf7
Henrique Pereira <henriquepbdev@gmail.com>
2020-01-28 18:22:43 -0300

    Updated all dependencies

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/todo');
+module.exports = mongoose.connect("mongodb://192.168.99.100:27017/todo", {
+  useMongoClient: true
+});
